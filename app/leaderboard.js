64a5b15d5ce2e4b4739909fd6bd9fa85144c21b2
Jipeng_Huang <jipenghuang@Jipengs-MacBook-Pro.local>
2016-10-11 22:25:04 -1000

    1

--- app/leaderboard.js
+++ app/leaderboard.js
@@ -1,3 +1,37 @@
 /**
  * Created by jipenghuang on 10/11/16.
  */
+playlists = new Mongo.Collection('players');
+
+if (Meteor.isClient) {
+  Template.leaderboard.helpers({
+    tasks: function() {
+      return playlists.find({}, {sort: {createdAt: -1}});
+    }
+  });
+  Template.leaderboard.events({
+    "submit .add-task": function(event) {
+      let name = event.target.name.value;
+      Meteor.call('addTask', name);
+      event.target.name.value = '';
+      return false;
+    },
+
+    'click': function(){
+      console.log("You clicked something");
+    }
+  });
+
+}
+
+Meteor.methods({
+  addTask: function(name){
+
+    playlists.insert({
+      name: name, createdAt: new Date()
+    });
+  },
+
+
+
+})
